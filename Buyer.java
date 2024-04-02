20ea0afe6154c9488e0d3b84326c142c6986c508
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-04-02 10:06:09 +0400

    added Buyer class

---
+++ Buyer.java
@@ -0,0 +1,12 @@
+package BentlyShowroom;
+
+public class Buyer {
+
+    Integer id;
+    String firstName;
+    String lastName;
+    Integer age;
+    String bankDetails;
+    Boolean isAllowed;
+
+}
